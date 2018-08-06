d40e2b492f66093dd7056dfe5b46f6afb9545de9
mina-gh <minamail@gmail.com>
2018-08-06 17:07:31 -0400

    worked on models - adding model definitions and relationships

--- models.js
+++ models.js
@@ -2,4 +2,47 @@ const Sequelize = require('sequelize');
 
 const db = new Sequelize('postgres://localhost:5432/plantr');
 
-module.exports = db;
+// gardener
+const Gardener = db.define('gardener', {
+  name: {
+    type: Sequelize.STRING
+  },
+  age: {
+    type: Sequelize.INTEGER
+  }
+});
+
+// plot
+const Plot = db.define('plot', {
+  size: {
+    type: Sequelize.INTEGER
+  },
+  shaded: {
+    type: Sequelize.BOOLEAN
+  }
+});
+
+// vegetable
+const Vegetable = db.define('vegetable', {
+  name: {
+    type: Sequelize.STRING
+  },
+  color: {
+    type: Sequelize.STRING
+  },
+  planted_on:{
+    type: Sequelize.DATE
+  }
+});
+
+Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
+
+Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'});
+Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'});
+
+Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'});
+
+
+
+module.exports = {db, Gardener, Plot, Vegetable};
